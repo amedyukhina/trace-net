96ccd21e25696e6f22ef971597571c299e07fba7
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-26 13:07:04 -0500

    rename

--- tracenet/utils/plot.py
+++ tracenet/utils/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pylab as plt
 
-from .utils import denormalize_points, cxcywh_to_xyxy
+from .points import denormalize_points, cxcywh_to_xyxy
 
 COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
           [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
