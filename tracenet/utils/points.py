fix angle calculation

tracenet/utils/points.py
@@ -1,7 +1,5 @@
 import torch
 
-from .utils import angle_between
-
 
 def xyxy_to_cxcywh(x):
     x0, y0, x1, y1 = x.unbind(-1)
@@ -33,7 +31,7 @@ def denormalize_points(x, size):
 
 def one_coord_to_al(x1, y1, x2, y2):
     l = torch.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    a = angle_between([y2 - y1, x2 - x1], [0, 1])
+    a = torch.atan((y2 - y1) / (x2 - x1))
     return a, l
 
 
@@ -41,3 +39,20 @@ def one_al_to_coord(x1, y1, a, l):
     x2 = x1 + l * torch.cos(a)
     y2 = y1 + l * torch.sin(a)
     return x2, y2
+
+
+def points_to_bounding_line(x):
+    points = x.unbind(-1)
+    y1, x1 = points[:2]
+    y2, x2 = points[-2:]
+    x_c = (x1 + x2) / 2
+    y_c = (y1 + y2) / 2
+    vec = torch.stack([y2 - y1, x2 - x1], dim=-1)
+    l = torch.norm(vec, dim=-1)
+    a = torch.atan(vec[:, 0] / vec[:, 1])
+    b = [y_c, x_c, l, a]
+    return torch.stack(b, dim=-1)
+
+#
+# def bounding_line_to_points(x):
+#     x_c, y_c, l, a = x.unbind(-1)