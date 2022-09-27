db763cbcb73b2feac59997d502127e5beb75c985
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-09-27 10:19:06 -0500

    normalize the distance calculation

--- tracenet/losses/symmetric_distance.py
+++ tracenet/losses/symmetric_distance.py
@@ -7,4 +7,5 @@ def symmetric_distance(source, target):
     with torch.no_grad():
         ls = F.mse_loss(sources, torch.stack([target, target]), reduction='none').sum(-1)
     ind = ls.argmin(0)
-    return F.mse_loss(sources[ind, torch.arange(len(ind))], target, reduction='none')
+    return torch.sqrt(((sources[ind, torch.arange(len(ind))] -
+                        target) ** 2).reshape(-1, 2).sum(-1)).reshape(-1, 2).sum(-1)
