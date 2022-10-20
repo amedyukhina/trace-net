96dc6dbb5a6606c3eddf15821038515e05462920
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-10-20 11:18:59 -0500

    Point loss

--- tracenet/losses/indexing.py
+++ tracenet/losses/indexing.py
@@ -3,6 +3,21 @@ import torch
 from torch.nn import functional as F
 
 
+class PointLoss(torch.nn.Module):
+
+    def __init__(self, dist_push_weight=1., intensity_weight=1., mindist=0.5, maxval=255):
+        super().__init__()
+        self.dist_push_weight = dist_push_weight
+        self.intensity_weight = intensity_weight
+        self.mindist = mindist
+        self.maxval = maxval
+
+    def forward(self, trace, img):
+        dist_push_loss = torch.stack([dist_push(tr, self.mindist) for tr in trace]).sum()
+        int_loss = torch.stack([intensity_loss(im, tr, self.maxval) for im, tr in zip(img, trace)]).sum()
+        return dist_push_loss * self.dist_push_weight + int_loss * self.intensity_weight
+
+
 def diff_index(x, idx):
     x = F.pad(x.unsqueeze(0).unsqueeze(0), pad=(0, 2, 0, 2), mode='reflect').squeeze(0).squeeze(0)
     i0 = (idx[0] + 0.5).floor().detach()
@@ -27,7 +42,7 @@ def diff_index(x, idx):
 
 
 def intensity_loss(img, trace, maxval=255):
-    intensity = diff_index(img, (trace * (torch.tensor(img.shape).unsqueeze(0))).transpose(0, 1))
+    intensity = diff_index(img, (trace * (torch.tensor(img.shape).to(img.device).unsqueeze(0))).transpose(0, 1))
     return (maxval - intensity).sum()
 
 
