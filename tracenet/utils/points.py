e1744042cd61c2f810daa945734eaae8768a56d6
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-11-14 15:15:56 -0600

    add a constant to prevent division by 0

--- tracenet/utils/points.py
+++ tracenet/utils/points.py
@@ -52,7 +52,9 @@ def point_segment_dist(v, w, p):
     Pytorch implementation of this solution: https://stackoverflow.com/a/1501725/13120052
     """
     l2 = ((w - v) ** 2).sum(-1)  # distance squared
-    t = ((p - v) * (w - v)).sum(-1) / l2  # relative projection of point p onto the segment
+    if l2 == 0:
+        return _dist(p, v)
+    t = ((p - v) * (w - v)).sum(-1) / (l2 + 10**(-10))  # relative projection of point p onto the segment
     t = torch.stack([torch.ones(len(t)).to(t.device), t]).min(0)[0]  # clamp the projection
     t = torch.stack([torch.zeros(len(t)).to(t.device), t]).max(0)[0]
     proj = v + t.unsqueeze(-1) * (w - v)
